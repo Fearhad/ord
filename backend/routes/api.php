90a5af1259363823aedfae9969c8e073afb301ef
Allan Hartigan <allan.hartigan@gmail.com>
2024-03-03 17:15:59 -0500

    init quasar and created the login and register apis

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// only routes that are not protected by sanctum
+Route::controller(AuthController::class)->group(function() {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login')
+})
